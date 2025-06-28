e362a5dfa98903415c9c1d8aae9168f4c5173a92
Jun Long <lingjunlong@u.nus.edu>
2025-06-29 01:48:22 +0800

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
 
 import notesRoutes from "../src/routes/notesRoutes.js";
 import { connectDB } from "../src/config/db.js";
@@ -11,9 +10,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
-const __filename = fileURLToPath(import.meta.url);
- const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 // middleware
 app.use(
@@ -27,13 +24,13 @@ app.use(express.json()); // JSON body parser
 // API routes only
 app.use("/api/notes", notesRoutes);
 
-// ❌ Remove frontend serving logic — you're deploying frontend separately
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 
 connectDB().then(() => {
