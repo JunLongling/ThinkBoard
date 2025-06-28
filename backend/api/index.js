6d5dce49f5900e4f733bcff0e27fbdb7afc5a8eb
Jun Long <lingjunlong@u.nus.edu>
2025-06-29 02:45:11 +0800

    test

--- backend/api/index.js
+++ backend/api/index.js
@@ -14,7 +14,7 @@ const __dirname = path.resolve();
 
 // middleware
 const corsConfig = {
-  origin: process.env.FRONTEND_URL, 
+  origin: "*", 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
@@ -22,7 +22,7 @@ const corsConfig = {
 app.use(cors(corsConfig));
 
 // Handle preflight requests
-app.options("*", cors(corsConfig));
+app.options("", cors(corsConfig));
 
 
 app.use(express.json()); // JSON body parser
